1069a00aa28dcc76ab82e1d146f278e841c64556
Kevin <kevit-development@users.noreply.github.com>
2017-08-04 17:07:55 +0200

    Create es.js

---
+++ locales/es.js
@@ -0,0 +1,7 @@
+// Spanish
+var locale = {
+	'shortMonths' : ['ene','feb','mar','abr','may','jun','jul','ago','sep','oct','nov','dic'],
+	'longMonths' : ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre'],
+	'shortDays' : ['lun','mar','mié','jue','vie','sáb','dom'],
+	'longDays' : ['lunes','martes','miércoles','jueves','viernes','sábado','domingo']
+};
